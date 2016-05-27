d1130be20a975038ed91bdf34fe8833f7f92f673
t-yamaguchi <t-yamaguchi@creationlinePC>
2016-05-27 15:05:15 +0900

    dsadad

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,15 @@
 require 'simplecov'
+require 'simplecov-json'
 require 'simplecov-rcov'
 
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::JSONFormatter,
   SimpleCov::Formatter::RcovFormatter
 ]
-SimpleCov.start
+SimpleCov.start do
+  add_filter "/spec/"
+end
 
 require_relative '../string_ops'
 require_relative '../string_ops2'
